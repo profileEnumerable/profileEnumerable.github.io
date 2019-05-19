c6c675ef797bbe0681a995bb604e588743e6ac46
profileEnumerable <45071639+profileEnumerable@users.noreply.github.com>
2019-05-19 13:01:41 +0300

    Delete textSaver.js

--- textSaver.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  const analysisButt = document.querySelector(".analysis-button");
-
-  analysisButt.addEventListener("click", function() {
-    var userText = document.querySelector("#typing-area").value;
-
-    sessionStorage.setItem("userText", userText);
-  });
-};
